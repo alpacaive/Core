5772e4d3f639ae75bddf944b82672acbc4697c4f
alpacaive <w6352800@naver.com>
2024-07-15 22:13:02 +0900

    스프링으로 전환하기

--- src/main/java/alpacaive/core/AppConfig.java
+++ src/main/java/alpacaive/core/AppConfig.java
@@ -9,16 +9,21 @@ import alpacaive.core.member.MemberServiceImpl;
 import alpacaive.core.member.MemoryMemberRepository;
 import alpacaive.core.order.OrderService;
 import alpacaive.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다
     // 생성한 객체 인스턴의 참조(래퍼런스)를 생성자를 통해 주입(연결)해준다
     // 객체의 생성과 연결은 AppConfig 가 담당
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountFolicy();
         return new RateDiscountPolicy(); // 할인 정책 변경
@@ -26,10 +31,12 @@ public class AppConfig {
     }
 
     // 생성자 주입
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     } // MemberServiceImpl 에는 MemoryMemberRepository 주입
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     } // OrderSErviceImpl 에는 MemoryMemberRepository, FixDiscountFolicy 주입
--- src/main/java/alpacaive/core/MemberApp.java
+++ src/main/java/alpacaive/core/MemberApp.java
@@ -4,12 +4,18 @@ import alpacaive.core.member.Grade;
 import alpacaive.core.member.Member;
 import alpacaive.core.member.MemberService;
 import alpacaive.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 얘가 스프링 컨테이너라고 보면 됨, @Bean 붙은거를 다 스프링 컨테이너에 넣어서 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
