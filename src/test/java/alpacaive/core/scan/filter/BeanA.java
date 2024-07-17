d6eabc434e874518c05c21c137b105343901fbdd
alpacaive <w6352800@naver.com>
2024-07-18 00:54:10 +0900

    필터 - includeFilters 와 excludeFilters 애노테이션 만들어서 필터 기능 테스트해보기

---
+++ src/test/java/alpacaive/core/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package alpacaive.core.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/alpacaive/core/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package alpacaive.core.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
