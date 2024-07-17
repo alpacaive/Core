09c8691402af2b235a526c3e06e2f2847535439e
alpacaive <w6352800@naver.com>
2024-07-18 04:10:36 +0900

    롬복 라이브러리 추가하고 사용해보기

--- src/main/java/alpacaive/core/order/OrderServiceImpl.java
+++ src/main/java/alpacaive/core/order/OrderServiceImpl.java
@@ -4,20 +4,17 @@ import alpacaive.core.discount.DiscountPolicy;
 import alpacaive.core.member.Member;
 import alpacaive.core.member.MemberRepository;
 import alpacaive.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 필수값인 파이널이 붙은 필드를 이용해 생성자를 만들어
 public class OrderServiceImpl implements OrderService {
 
     // DIP 지킴: 구체화에 의존하지 않고 추상화에만 의존
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
