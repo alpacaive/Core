92d8a97ad687064ed9cf1641875c382c89e503f6
alpacaive <w6352800@naver.com>
2024-07-18 19:54:16 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/alpacaive/core/discount/FixDiscountFolicy.java
+++ src/main/java/alpacaive/core/discount/FixDiscountFolicy.java
@@ -2,7 +2,9 @@ package alpacaive.core.discount;
 
 import alpacaive.core.member.Grade;
 import alpacaive.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountFolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 1000원 할인
