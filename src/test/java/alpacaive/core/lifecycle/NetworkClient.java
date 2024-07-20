d3cb6136126dc33fd72595598b477ffdc8176cb7
alpacaive <w6352800@naver.com>
2024-07-20 21:22:13 +0900

    빈 등록 초기화, 소멸 메서드

--- src/test/java/alpacaive/core/lifecycle/NetworkClient.java
+++ src/test/java/alpacaive/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package alpacaive.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,16 +28,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close = " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { // 의존관계 주입이 끝나면 호출해주는
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception { // 의존관계 주입이 끝나면 호출해주는
+        System.out.println("NetworkClient.init");
         connect();
         call("최기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
